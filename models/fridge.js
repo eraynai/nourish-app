0701149905015b2f61bfbc12c9e0b73b664bfda3
Elli Raynai <elli@elliraynai.com>
2021-08-17 15:26:03 -0400

    added basic user schema and fridge schema

---
+++ models/fridge.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const fridgeSchema = new Schema(
+	{
+		name: String,
+		lat: Number,
+		lng: Number,
+		time: Date,
+		date: Date,
+		description: String,
+		user: { type: Schema.Types.ObjectId, ref: 'User' },
+	},
+	{
+		timestamps: true,
+	}
+);
+
+module.exports = mongoose.model('Fridge', fridgeSchema);
