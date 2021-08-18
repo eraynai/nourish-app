928452cbfb27dc9a3fbe3c97da6de3a50fd43aac
Elli Raynai <elli@elliraynai.com>
2021-08-18 09:01:43 -0400

    created form for fridge

--- src/components/FridgeForm/FridgeForm.js
+++ src/components/FridgeForm/FridgeForm.js
@@ -1,33 +1,93 @@
-import React, { Component, useState } from 'react';
-import DatePicker from 'react-datepicker';
-// export default class FridgeForm extends Component {
+import React, { Component } from 'react';
 
-//     state = {
-//         name: '',
-//         address: '',
-//         description: '',
-//         imageUrl: '',
-//     }
+export default class FridgeForm extends Component {
+	state = {
+		name: '',
+		address: '',
+		description: '',
+		date: '',
+	};
 
-// 	render() {
-// 		return (
-// 			<React.Fragment>
-// 				<h1>Test</h1>
-// 			</React.Fragment>
-// 		);
-// 	}
-// }
+	addFridge = async (e) => {
+		e.preventDefault();
+		try {
+			let jwt = localStorage.getItem('token');
+			let fetchResponse = await fetch('/api/fridges', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: 'Bearer ' + jwt,
+				},
+				body: JSON.stringify({
+					name: this.state.name,
+					address: this.state.address,
+					lat: this.props.event.lat,
+					lng: this.props.event.lng,
+					time: this.props.event.time,
+					date: this.state.date,
+					description: this.state.description,
+				}),
+			});
+			await fetchResponse.json();
+			this.props.updateMarker({
+				name: this.state.name,
+				address: this.state.address,
+				lat: this.props.event.lat,
+				lng: this.props.event.lng,
+				date: this.state.date,
+				description: this.state.description,
+			});
+			this.setState({
+				name: '',
+				address: '',
+				date: '',
+				description: '',
+			});
+		} catch (err) {
+			console.log('Error:', err);
+		}
+	};
 
-export default function FridgeForm() {
-	const [name, setName] = useState('');
-	const [address, setAddress] = useState('');
-	const [description, setDescription] = useState('');
-	const [date, setDate] = useState(new Date());
-	const [image, setImage] = useState('');
+	handleChange = (e) => {
+		this.setState({ [e.target.name]: e.target.value });
+	};
 
-	return (
-		<React.Fragment>
-			<h1>Test</h1>
-		</React.Fragment>
-	);
+	render() {
+		return (
+			<React.Fragment>
+				<form>
+					<input
+						placeholder='Enter Your Company Name'
+						type='text'
+						name='name'
+						value={this.state.name}
+						onChange={this.handleChange}
+					/>
+					<input
+						placeholder='Enter Fridge Address'
+						type='text'
+						name='location'
+						value={this.state.location}
+						onChange={this.handleChange}
+					/>
+					<input
+						placeholder='Enter Date of Food Drop Off'
+						type='datetime-local'
+						value={this.state.date}
+						name='date'
+						onChange={this.handleChange}
+					/>
+					<textarea
+						placeholder='Enter Description of Food'
+						type='text-area'
+						name='description'
+						value={this.state.description}
+						onChange={this.handleChange}
+					></textarea>
+					<br></br>
+					<button onClick={this.addFridge}>Add Fridge</button>
+				</form>
+			</React.Fragment>
+		);
+	}
 }
